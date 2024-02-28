3ae3d8ae85447ff9e0c2d494796bef4648706de7
Athlie54 <kjself04@gmail.com>
2024-02-28 14:53:31 -0600

    Pizza constructor and getters/setters

--- Homework5/src/pizza/AbstractPizza.java
+++ Homework5/src/pizza/AbstractPizza.java
@@ -1,5 +1,66 @@
 package pizza;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AbstractPizza {
 
+    protected double priceWithoutToppings;
+    protected double totalPrice;
+    protected int pizzaOrderID;
+    protected ICookingStrategy cookingStrategy;
+    protected double cookingPrice;
+    protected List<Toppings> toppingsList;
+
+    public AbstractPizza() {
+        this.toppingsList= new ArrayList<Toppings>();
+    }
+    
+    public List<Toppings> getToppingsList() {
+        return toppingsList;
+    }
+
+    public void setToppingsList(List<Toppings> toppingsList) {
+        this.toppingsList = toppingsList;
+    }
+
+    public double getPriceWithoutToppings() {
+        return priceWithoutToppings;
+    }
+
+    public void setPriceWithoutToppings(double priceWithoutToppings) {
+        this.priceWithoutToppings = priceWithoutToppings;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getPizzaOrderID() {
+        return pizzaOrderID;
+    }
+
+    public void setPizzaOrderID(int pizzaOrderID) {
+        this.pizzaOrderID = pizzaOrderID;
+    }
+
+    public ICookingStrategy getCookingStrategy() {
+        return cookingStrategy;
+    }
+
+    public void setCookingStrategy(ICookingStrategy cookingStrategy) {
+        this.cookingStrategy = cookingStrategy;
+    }
+
+    public double getCookingPrice() {
+        return cookingPrice;
+    }
+
+    public void setCookingPrice(double cookingPrice) {
+        this.cookingPrice = cookingPrice;
+    }
 }
