a1779427fe41bf9e77d1cbcb6b2d48a0d4628392
RougeAce <Trey722@users.noreply.github.com>
2024-02-28 22:15:15 -0600

    Aded BrickOverNCookingSTartgey

--- Homework5/src/pizza/BrickOvenCookingStrategy.java
+++ Homework5/src/pizza/BrickOvenCookingStrategy.java
@@ -1,5 +1,18 @@
 package pizza;
 
-public class BrickOvenCookingStrategy {
+public class BrickOvenCookingStrategy implements ICookingStrategy {
+
+
+    //Takes a pizza as input and then stes its cooking strartgey to current, updates cooking price and totalPrice 
+    @Override
+    public boolean cook(AbstractPizza pizza)
+    {
+        
+        pizza.setCookingStrategy(this);
+        pizza.setCookingPrice(10.0);
+        pizza.setTotalPrice(pizza.getTotalPrice() + pizza.getCookingPrice());
+        return true;
+       
+    }
 
 }
