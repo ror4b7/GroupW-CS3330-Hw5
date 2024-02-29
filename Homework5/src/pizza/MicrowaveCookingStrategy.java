a6711457ec0334509be9816a6629b607426a19dc
RougeAce <Trey722@users.noreply.github.com>
2024-02-28 22:19:13 -0600

    Added cooking startS

--- Homework5/src/pizza/MicrowaveCookingStrategy.java
+++ Homework5/src/pizza/MicrowaveCookingStrategy.java
@@ -1,5 +1,17 @@
 package pizza;
 
-public class MicrowaveCookingStrategy {
+public class MicrowaveCookingStrategy  implements ICookingStrategy
+{
 
+    //Takes a pizza as input and then stes its cooking strartgey to current, updates cooking price and totalPrice 
+    @Override
+    public boolean cook(AbstractPizza pizza)
+    {
+        
+        pizza.setCookingStrategy(this);
+        pizza.setCookingPrice(1.0);
+        pizza.setTotalPrice(pizza.getTotalPrice() + pizza.getCookingPrice());
+        return true;
+       
+    }
 }
